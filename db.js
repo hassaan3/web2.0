262aca9ec2d7524bf118a6482c4d6107c72c7764
hassaan.rana@obcc.de <hassaanahmedrana@outlook.com>
2018-09-21 15:58:25 +0200

    Connecting MySQL

---
+++ db.js
@@ -0,0 +1,26 @@
+var mysql = require('mysql');
+var db_config = {
+  host: 'localhost',
+  user: 'root',
+  password: 'OBCCpassword',
+  database: 'webapp'
+}
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config);
+  connection.connect((err) => {
+    if (err) {
+      console.error('error connecting to db: ', err);
+      setTimeout(handleDisconnect, 2000);
+    }
+
+    console.log('connection established');
+  });
+
+  connection.on('error', (err) => {
+    if(err.code == 'PROTOCOL_CONNECTION_LOST')
+      handleDisconnect();
+    else
+      throw err;
+  });
+}
