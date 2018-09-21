648f8bc49e9342d1b8a9bf1a3bdadfa6069d649d
hassaan.rana@obcc.de <hassaanahmedrana@outlook.com>
2018-09-21 15:07:18 +0200

    Validation Message

--- server.js
+++ server.js
@@ -11,7 +11,15 @@ app.listen(3000, () => {
 app.post('/', (req, res) => {
   var email = req.body.email;
   var amount = req.body.amount;
-  
+
+  if (amount <= 1) {
+    return_info = new Object();
+    return_info.error = true;
+    return_info.message = 'The amount should be greater than 1';
+
+    res.send(return_info);
+  }
+
   res.send({'email' : email ,'amount' : amount})
 })
 
